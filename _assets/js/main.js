13926fc6a997fdbc9643079ea2e062c80d43fd5c
Andy Gordon <andy@kujilabs.com>
2016-09-28 18:49:51 +0100

    cloned

---
+++ _assets/js/main.js
@@ -0,0 +1,17 @@
+//= require jquery.min
+//= require jquery.easing
+//= require jquery-ui.min
+//= require bootstrap
+//= require jquery.flexslider
+//= require background-check.min
+//= require jquery.viewportchecker
+//= require jquery.stellar.min
+//= require wow.min
+//= require owl.carousel.min
+//= require imagesloaded.pkgd.min
+//= require jPushMenu
+//= require jquery.fs.tipper.min
+//= require domain
+//= require TweenLite.min
+//= require EasePack.min
+//= require pollyfill
